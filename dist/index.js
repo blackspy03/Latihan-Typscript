4c956af5b50886920df9b895ed75c76236462c91
blackspy03 <106814890+blackspy03@users.noreply.github.com>
2025-10-23 18:31:07 +0700

    basic typescript explanation

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let namaSaya = "John Doe";
+namaSaya = 123;
+console.log({ namaSaya });
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let namaSaya: string = "John Doe";
+let umurSaya = 30; //(jadi number, menyesuaikan first declaration) 
+
+let isMarried: any; //bisa diisi dengan tipe data apa saja
+
+//array 
+let saudaraSaya: string[] = ["Jane", "Doe"]; //di isi hanya dengan string
+
+//tuple array
+let alamatSaya: [string, number] = ["Jl. Merdeka", 12345]; //di isi dengan string dan number sesuai urutan 
+
+
+
+console.log({ namaSaya }); //menampilkan dalam bentuk object
+console.log(namaSaya); //menampilkan dalam bentuk string biasa
